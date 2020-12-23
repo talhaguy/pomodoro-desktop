0b494e9d359e03ba7fdc910ec098a26b5c8bddd4
Muhammad Talha <talhaguy@gmail.com>
2020-12-22 17:44:08 -0800

    [feature/PA-4] starting timer and progress visual components

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,24 +1,134 @@
-import React from "react";
+import React, { MutableRefObject, useEffect, useRef } from "react";
 import { connect, useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import { increment, decrement } from "./counterSlice";
 
 // Counter
 
-function Counter({ count, increment, decrement }) {
+export function degreesToRadians(degrees: number) {
+    return (Math.PI / 180) * degrees;
+}
+
+export type Coordinates = [x: number, y: number];
+
+export function calcCoordsForPointOnCirclePerimeter(
+    centerOfCircle: Coordinates,
+    radius: number,
+    degrees: number
+): Coordinates {
+    const radians = degreesToRadians(degrees);
+    const x = centerOfCircle[0] + radius * Math.cos(radians);
+    const y = centerOfCircle[1] + radius * Math.sin(radians);
+    return [x, y];
+}
+
+export function useRenderProgressCircleInCanvas(
+    canvasWidth: number,
+    canvasHeight: number,
+    circleWidth: number,
+    current: number,
+    total: number
+) {
+    const canvasRef = useRef<HTMLCanvasElement>();
+
+    useEffect(() => {
+        const percentage = current / total;
+        const degrees = 360 * percentage;
+        const degreesOffset = degrees - 90;
+        const radians = degreesToRadians(degreesOffset);
+
+        const context = canvasRef.current.getContext("2d");
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+        // render circle
+        context.beginPath();
+        context.arc(
+            canvasWidth / 2,
+            canvasHeight / 2,
+            circleWidth / 2,
+            degreesToRadians(0),
+            degreesToRadians(360)
+        );
+        context.strokeStyle = "lightblue";
+        context.lineWidth = 2;
+        context.stroke();
+        context.closePath();
+
+        // render progress
+        context.beginPath();
+        context.arc(
+            canvasWidth / 2,
+            canvasHeight / 2,
+            circleWidth / 2,
+            degreesToRadians(-90),
+            radians
+        );
+        context.strokeStyle = "blue";
+        context.lineWidth = 2;
+        context.stroke();
+        context.closePath();
+
+        // render nib
+        const nibCoords = calcCoordsForPointOnCirclePerimeter(
+            [canvasWidth / 2, canvasHeight / 2],
+            circleWidth / 2,
+            degreesOffset
+        );
+        context.beginPath();
+        context.arc(
+            nibCoords[0],
+            nibCoords[1],
+            4,
+            degreesToRadians(0),
+            degreesToRadians(360)
+        );
+        context.fillStyle = "blue";
+        context.fill();
+        context.closePath();
+    });
+
+    return canvasRef;
+}
+
+export interface ProgressCircleProps {
+    current: number;
+    total: number;
+}
+
+export function ProgressCircle({ current, total }: ProgressCircleProps) {
+    const WIDTH = 300;
+    const HEIGHT = 300;
+
+    const canvasRef = useRenderProgressCircleInCanvas(
+        WIDTH,
+        HEIGHT,
+        WIDTH - 10,
+        current,
+        total
+    );
+
+    return <canvas ref={canvasRef} width={WIDTH} height={HEIGHT} />;
+}
+
+export interface TimeProps {
+    ms: number;
+}
+
+function Time({ ms }: TimeProps) {
     return (
         <div>
-            <span>{count}</span>
-            <button onClick={increment}>+</button>
-            <button onClick={decrement}>-</button>
+            <div>{ms}</div>
+            <ProgressCircle current={ms} total={25 * 60 * 1000} />
         </div>
     );
 }
 
 // App
 
-const AppStyled = styled.div`
-    color: blue;
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: white;
+    }
 `;
 
 export function App() {
@@ -28,13 +138,9 @@ export function App() {
     const dispatch = useDispatch();
 
     return (
-        <AppStyled>
-            <h1>This is an App</h1>
-            <Counter
-                count={count}
-                increment={() => dispatch(increment())}
-                decrement={() => dispatch(decrement())}
-            />
-        </AppStyled>
+        <>
+            <GlobalStyle />
+            <Time ms={(25 * 60 * 1000) / 2} />
+        </>
     );
 }
