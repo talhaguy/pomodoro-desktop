42b981b0d085f03e9aa405a94b363e5a056a867b
Muhammad Talha <talhaguy@gmail.com>
2020-12-26 11:40:45 -0800

    [feature/PA-32] add comment in preload file

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,5 +1,4 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
 import { Environment } from "../shared/environment";
 
+// make global variables available to renderer
 (global as any).ENV = (process.env.ENV as Environment) || Environment.Prod;
