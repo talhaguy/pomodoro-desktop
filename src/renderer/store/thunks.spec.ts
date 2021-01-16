f2b297d3e07b50e5b17751fef08888dd31dee69c
Muhammad Talha <talhaguy@gmail.com>
2021-01-15 18:50:25 -0800

    [feature/PA-41] adjust startTimerAnimation test to exclude dispatch checking; add startTimer tests

--- src/renderer/store/thunks.spec.ts
+++ src/renderer/store/thunks.spec.ts
@@ -1,36 +1,47 @@
 import { IntervalType } from "../interval";
 import { RootState } from "./store";
-import { startTimerAnimation } from "./thunks";
+import { startTimer, startTimerAnimation } from "./thunks";
 
 jest.setTimeout(10000);
+// jest.mock("react-redux", () => {
+//     const mockRedux = {
+//         batch: (cb) => {
+//             cb();
+//         },
+//     };
+//     return mockRedux;
+// });
 
 describe("thunks", () => {
+    let draw;
+    let dispatch;
+    let rootState: RootState;
+    let getState;
+
+    beforeEach(() => {
+        draw = jest.fn();
+        dispatch = jest.fn();
+        rootState = {
+            timer: {
+                time: 0,
+                active: false,
+                intervalType: IntervalType.Focus,
+                numFocusIntervalsCompleted: 0,
+                skip: false,
+                reset: false,
+            },
+        };
+        getState = jest.fn().mockReturnValue(rootState);
+        jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        jest.clearAllTimers();
+        jest.useRealTimers();
+    });
+
     describe("startTimerAnimation()", () => {
-        let draw;
-        let dispatch;
-        let rootState: RootState;
-        let getState;
-
-        beforeEach(() => {
-            draw = jest.fn();
-            dispatch = jest.fn();
-            rootState = {
-                timer: {
-                    time: 0,
-                    active: false,
-                    intervalType: IntervalType.Focus,
-                    numFocusIntervalsCompleted: 0,
-                    skip: false,
-                    reset: false,
-                },
-            };
-            getState = jest.fn().mockReturnValue(rootState);
-        });
-
-        afterEach(() => {
-            jest.clearAllMocks();
-        });
-
         it("should break out of raf loop if skip flag is set", (done) => {
             rootState.timer.active = true;
             rootState.timer.skip = true;
@@ -42,10 +53,6 @@ describe("thunks", () => {
             )(dispatch, getState);
 
             promise.then((time) => {
-                expect(dispatch.mock.calls[0][0].type).toBe(
-                    "timer/nextInterval"
-                );
-                expect(dispatch).toHaveBeenCalledTimes(1);
                 expect(draw).toHaveBeenCalledWith(0);
                 expect(draw).toHaveBeenCalledTimes(1);
                 expect(time).toBe(0);
@@ -64,15 +71,45 @@ describe("thunks", () => {
             )(dispatch, getState);
 
             promise.then((time) => {
-                expect(dispatch.mock.calls[0][0].type).toBe(
-                    "timer/resetInterval"
-                );
-                expect(dispatch).toHaveBeenCalledTimes(1);
                 expect(draw).toHaveBeenCalledWith(0);
                 expect(draw).toHaveBeenCalledTimes(1);
                 expect(time).toBe(0);
                 done();
             });
+        });
+    });
+
+    describe("startTimer", () => {
+        it("should return an `setInterval` id", () => {
+            const total = 5000;
+            const id = startTimer(total, () => {})(dispatch, getState);
+            expect(typeof id).toBe("number");
+        });
+
+        it("should increment the timer every second", () => {
+            const total = 5000;
+            startTimer(total, () => {})(dispatch, getState);
+            jest.advanceTimersByTime(5000);
+            const countIncrement = dispatch.mock.calls.filter(
+                (call) => call[0].type === "timer/increment"
+            ).length;
+            expect(countIncrement).toBe(5);
+        });
+
+        it("should stop the timer at the end of an interval", () => {
+            rootState.timer.time = 5000;
+            const total = 5000;
+            const cb = jest.fn();
+            startTimer(total, cb)(dispatch, getState);
+            jest.advanceTimersByTime(1000);
+            const nextIntervalCalled =
+                dispatch.mock.calls.findIndex(
+                    (call) => call[0].type === "timer/nextInterval"
+                ) > -1;
+            expect(nextIntervalCalled).toBeTruthy();
+            dispatch.mock.calls[2][0]();
+            expect(jest.getTimerCount()).toBe(0);
+            expect(cb).toBeCalled();
         });
     });
 });
